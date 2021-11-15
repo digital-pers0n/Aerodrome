c9f53e39c73c5499dc07e02fb6c78ac5717dc0e6
digital-pers0n <aquaphonic@gmail.com>
2021-11-15 18:35:28 +0300

    Update project

--- Aerodrome/AppDelegate.h
+++ Aerodrome/AppDelegate.h
@@ -2,56 +2,12 @@
 //  AppDelegate.h
 //  Aerodrome
 //
-//  Created by Terminator on 7/22/16.
-//
+//  Created by Maxim M. on 11/13/21.
 //
 
 #import <Cocoa/Cocoa.h>
 
-
-
-@class CWInterface, AERHelper, AERController, AERStatusMenuItemView, AERWLClient;
-//@class AERMenu, AERProgressIndicator;
-
-@interface AppDelegate : NSObject <NSApplicationDelegate, NSMenuDelegate>
-{
-    
-   // AERHelper *helper;
-    dispatch_queue_t execute_queue;
-    dispatch_source_t _updateTimer;
-    dispatch_source_t _activityTimer;
-    //BOOL    _shouldStartScanUpdateTimer;
-    BOOL    _isMenuOpen;
-    BOOL    _isAssociated;
-    BOOL    _isPowered;
-    
-    //Menu
-    NSStatusItem *menuStatusBarItem;
-    
-    NSMenu  *menu;
-    
-    AERStatusMenuItemView  *statusMenuItem;
-    
-    //Daemon
-//    NSXPCConnection *connection;
-    //AERWLInterface  *interface;
-    AERController *interface;
-    
-    AERWLClient *client;
-    
-    NSArray *networks;
-    
-    NSDictionary *menuBarIcons;
-    
-    //CoreWlan
-    CWInterface *coreWlanIterface;
-    
-    //Dialogs
-    //NSWindowController *currentDialog;
-    
-}
-
--(void)networkItemClicked:(id)sender;
+@interface AppDelegate : NSObject <NSApplicationDelegate>
 
 
 @end
--- Aerodrome/main.m
+++ Aerodrome/main.m
@@ -2,26 +2,14 @@
 //  main.m
 //  Aerodrome
 //
-//  Created by Terminator on 7/22/16.
-//
+//  Created by Maxim M. on 11/13/21.
 //
 
 #import <Cocoa/Cocoa.h>
-#import "AppDelegate.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        ProcessSerialNumber psn = {0, kCurrentProcess};
-        
-        TransformProcessType(&psn, kProcessTransformToUIElementApplication);
-//        NSApplication *app = [NSApplication sharedApplication];
-//        AppDelegate * del = [[AppDelegate alloc] init];
-//        [app setDelegate:del];
-//        
-//        [app run];
-//        return 0;
-    
-
-     return NSApplicationMain(argc, argv);
+        // Setup code that might create autoreleased objects goes here.
     }
+    return NSApplicationMain(argc, argv);
 }
