6e177db16a916e48705129848f3ac1d840343618
digital-pers0n <aquaphonic@gmail.com>
2021-12-19 18:25:42 +0300

    Add StatusView class

---
+++ Aerodrome/AERKit/AERStatusView.h
@@ -0,0 +1,18 @@
+//
+//  AERStatusView.h
+//  Aerodrome
+//
+//  Created by Maxim M. on 12/15/21.
+//
+
+#import <Cocoa/Cocoa.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AERStatusView : NSView
+
+@property (nonatomic, copy) NSString *statusText;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Aerodrome/AERKit/AERStatusView.mm
@@ -0,0 +1,64 @@
+//
+//  AERStatusView.mm
+//  Aerodrome
+//
+//  Created by Maxim M. on 12/15/21.
+//
+
+#import "AERStatusView.h"
+
+[[clang::objc_direct_members]]
+@implementation AERStatusView {
+    NSTextFieldCell *_statusText;
+    NSProgressIndicator *_progressIndicator;
+}
+
+//MARK: - Initializers
+
+- (instancetype)initWithFrame:(NSRect)frameRect {
+    if (!(self = [super initWithFrame:frameRect])) return self;
+    self.autoresizingMask = NSViewHeightSizable | NSViewWidthSizable;
+    
+    auto tfc = [[NSTextFieldCell alloc] initTextCell:@""];
+    tfc.font = [NSFont menuBarFontOfSize:14];
+    tfc.textColor = NSColor.disabledControlTextColor;
+    
+    auto pi = [NSProgressIndicator new];
+    pi.style = NSProgressIndicatorStyleSpinning;
+    pi.displayedWhenStopped = NO;
+    pi.controlSize = NSControlSizeSmall;
+    pi.autoresizingMask = NSViewMaxXMargin;
+    pi.translatesAutoresizingMaskIntoConstraints = YES;
+    pi.autoresizesSubviews = YES;
+    pi.usesThreadedAnimation = YES;
+    [self addSubview:pi];
+    _progressIndicator = pi;
+    
+    return self;
+}
+
+- (instancetype)initWithCoder:(NSCoder *)coder {
+    [[NSException exceptionWithName:NSInternalInconsistencyException
+                             reason:@"Not implemented" userInfo:nil] raise];
+    return nil;
+}
+
+//MARK: - Properties
+
+- (void)setStatusText:(NSString *)text {
+    _statusText.stringValue = text;
+}
+
+- (NSString *)statusText {
+    return _statusText.stringValue;
+}
+
+//MARK: - Drawing
+
+- (void)drawRect:(NSRect)dirtyRect {
+    const auto width = NSWidth(self.frame);
+    _progressIndicator.frame = {{width - 26, 2}, {16, 16}};
+    [_statusText drawWithFrame:{{19, 0}, {width - 60, 19}} inView:self];
+}
+
+@end
